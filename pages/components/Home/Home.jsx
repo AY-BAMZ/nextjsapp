9652d193fb4358100955c4822e80000d27217cb3
AY-BAMZ <aybamz181@gmail.com>
2022-01-18 22:17:11 +0100

    yea

--- pages/components/Home/Home.jsx
+++ pages/components/Home/Home.jsx
@@ -4,19 +4,29 @@ import styles from "../../../styles/components/Home/Home.module.css"
 
 function Home() {
   const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postPerPage, setPostPerPage] = useState(4)
+
+
   useEffect(() => {
     const url = "https://jsonplaceholder.typicode.com/photos";
     fetch(url)
       .then((resp) => resp.json())
       .then((resp) => setPosts(resp));
   }, []);
+
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
+
   return (
     <div>
       <div className={styles.posts}>
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
           <Post title={post.title} thumbnailUrl={post.thumbnailUrl} key={post.id} />
         ))}
       </div>
+      <button onClick={() => setPostPerPage(postPerPage + 4)} className={styles.button}>Load More</button>
     </div>
   );
 }
--- pages/components/Home/Post.jsx
+++ pages/components/Home/Post.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from "../../../styles/components/Home/Post.module.css"
+import styles from "../../../styles/components/Home/Post.module.css";
 
 function Post({ title, thumbnailUrl, id }) {
   return (
     <div className={styles.container}>
-       <img src={thumbnailUrl} alt="" />
-        <h2>{title}</h2>
+      <img src={thumbnailUrl} alt="" />
+      <h2>{title}</h2>
     </div>
   );
 }
